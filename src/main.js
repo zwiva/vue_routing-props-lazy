90e52b4b07960a56638d11d85e3a114a09ee6707
Andrea Castro <ap.castrocastellon@gmail.com>
2021-09-17 05:50:33 -0300

    change base project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 window.$ = window.jQuery = require('jquery')
-import 'bootstrap'
+import 'bootstrap';
+import router from './router';
 
 Vue.config.productionTip = false
 
